ffb6d7fe5f7d4853515dab388fc6ee0b61052b54
Yonatan Wolowelsky <grmmph@gmail.com>
2015-11-28 16:39:50 +0100

    handling ids from server

--- frontend/Game.js
+++ frontend/Game.js
@@ -1,18 +1,14 @@
 var gameSize = [1000, 800];
-var game = new Phaser.Game(gameSize[0], gameSize[1], Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(gameSize[0], gameSize[1], Phaser.AUTO, '', { preload: preload, create: beforeCreate, update: update });
 var playerId;
 var radius = 130;
 var planets;
 var planet1;
 var planet2;
+var userPlanet;
 var p1_astroids;
 var p2_astroids;
-var aim1;
-var aim2;
-
-Requester.signIn(function () {
-	playerId = Requester.playerId;
-});
+var aim;
 
 function preload() {
     game.load.image('background', 'assets/background.png');
@@ -20,23 +16,49 @@ function preload() {
     game.load.image('planet2', 'assets/planet2.png');
     game.load.image('aim', 'assets/aim.png');
     game.load.image('astroid', 'assets/astroid_small.png');
-}
+};
+
+function createPlanet() {
+	planets = game.add.group();
+	planets.enableBody = true;
+	planet1 = planets.create(100, 300, 'planet1');
+	planet2 = planets.create(800, 300, 'planet2');
+	if (Requester.playerId == 0) {
+		userPlanet = planet1;
+	} else if (Requester.playerId == 1) {
+		userPlanet = planet2;
+	}
+};
+
+function createAim() {
+	aim = game.add.sprite(userPlanet.x + (userPlanet.width/2), userPlanet.y + (userPlanet.height/2), 'aim');
+	aim.anchor.setTo(0.5, 0.5);
+
+	game.input.keyboard.onUpCallback = function( e ) {
+	    if(e.keyCode == Phaser.Keyboard.UP){
+	        aim.angle -= 1;
+	    }
+			if(e.keyCode == Phaser.Keyboard.DOWN){
+	        aim.angle += 1;
+	    }
+	};
+};
+
+function beforeCreate(argument) {
+	Requester.signIn(function () {
+		create();
+	});
+};
 
 function create() {
-
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     //  A simple background for our game
     var background = game.add.tileSprite(0, 0, gameSize[0], gameSize[1], "background");
+		createPlanet();
+		createAim();
 
-    //  The planets group contains the planets (at least two)
-    planets = game.add.group();
-    planets.enableBody = true;
-    planet1 = planets.create(100, 300, 'planet1');
-    planet2 = planets.create(800, 300, 'planet2');
-    aim1 = game.add.sprite(planet1.x + (planet1.width/2), planet1.y + (planet1.height/2), 'aim');
-		aim1.anchor.setTo(0.5, 0.5);
     //  The astroids group contains the planets (at least two)
     p1_astroids = game.add.group();
     p1_astroids.enableBody = true;
@@ -53,23 +75,7 @@ function create() {
     }
 
     var astroid = p1_astroids.create(50, 120, 'astroid');
-
-    var planet2 = planets.create(800, 300, 'planet');
-
-    //  We need to enable physics on the player
-    //~ game.physics.arcade.enable(player);
-
-
-		//events
-		game.input.keyboard.onUpCallback = function( e ){
-        if(e.keyCode == Phaser.Keyboard.UP){
-            aim1.angle -= 1;
-        }
-				if(e.keyCode == Phaser.Keyboard.DOWN){
-            aim1.angle += 1;
-        }
-    };
-}
+};
 
 function update() {
     //  Collide the player and the stars with the platforms
