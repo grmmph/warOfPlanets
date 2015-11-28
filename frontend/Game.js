2a3362ab8676f47f0afccae4565fcd35fd4e5738
neiman30 <neiman.mail@gmail.com>
2015-11-28 15:28:47 +0100

    astroid player 1, colliding

--- frontend/Game.js
+++ frontend/Game.js
@@ -1,7 +1,14 @@
+// consts
+var radius = 80;
+var planet1x = 800;
+var planet1y = 300;
+var planet2x = 300;
+var planet2y = 300;
+var astroid_perimeter = 30;
+
 var gameSize = [1000, 800];
 var game = new Phaser.Game(gameSize[0], gameSize[1], Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var playerId;
-var radius = 130;
 var planets;
 var p1_astroids;
 var p2_astroids;
@@ -31,29 +38,27 @@ function create() {
     //  The planets group contains the planets (at least two)
     planets = game.add.group();
     planets.enableBody = true;
-    var planet1 = planets.create(100, 300, 'planet1');
-    var planet2 = planets.create(800, 300, 'planet2');
+    var planet1 = planets.create(planet1x, planet1y, 'planet1');
+    var planet2 = planets.create(planet2x, planet1y, 'planet2');
 
     aim1 = game.add.sprite(planet1.x + (planet1.width/2), planet1.y + (planet1.height/2), 'aim');
 		aim1.anchor.setTo(0.5, 0.5);
     //  The astroids group contains the planets (at least two)
     p1_astroids = game.add.group();
     p1_astroids.enableBody = true;
-    for (var i = 0; i < 10; i++)
+    for (var i = 0; i < 7; i++)
     {
         //  Create an random astroid
-        var ranpos_x = Math.random()*radius*2 << 0;
-        var ranpos_ysign = Math.random()*1 << 0;
+        var ranpos_x = (Math.random()*radius*2 << 0) - radius;
+        var ranpos_ysign = Math.random()*2 << 0;
         if (ranpos_ysign == 0) {ranpos_ysign = -1;}
 
         pos_y = Math.floor( Math.sqrt( radius*radius - ranpos_x*ranpos_x ) );
 
-        var astroid = p1_astroids.create(ranpos_x, ranpos_ysign*pos_y, 'astroid');
+        var astroid = p1_astroids.create(planet1x + astroid_perimeter + ranpos_x,planet1y + 35 + ranpos_ysign*pos_y, 'astroid');
     }
 
-    var astroid = p1_astroids.create(50, 120, 'astroid');
 
-    var planet2 = planets.create(800, 300, 'planet');
 
     //  We need to enable physics on the player
     //~ game.physics.arcade.enable(player);
