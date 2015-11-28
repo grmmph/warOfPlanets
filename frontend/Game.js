bfe1e8bada290355bf4003f024b7502422fd8703
neiman30 <neiman.mail@gmail.com>
2015-11-28 15:01:43 +0100

    add astroids

--- frontend/Game.js
+++ frontend/Game.js
@@ -7,12 +7,17 @@ Requester.signIn(function () {
 function preload() {
     game.load.image('background', 'assets/background.png');
     game.load.image('planet', 'assets/planet.png');
-    game.load.image('astroid', 'assets/astroid.png');
+    game.load.image('astroid', 'assets/astroid_small.png');
 }
 
+// game consts
+var radius = 130;
+
 var planets;
-var astroids;
-var aims;
+var p1_astroids;
+var p2_astroids;
+var aim1;
+var aim2;
 
 function create() {
 
@@ -27,6 +32,24 @@ function create() {
     planets.enableBody = true;
     var planet = planets.create(100, 300, 'planet');
     var planet = planets.create(800, 300, 'planet');
+    
+    //  The astroids group contains the planets (at least two)
+    p1_astroids = game.add.group();
+    p1_astroids.enableBody = true;
+    for (var i = 0; i < 10; i++)
+    {
+        //  Create an random astroid
+        var ranpos_x = Math.random()*radius*2 << 0;
+        var ranpos_ysign = Math.random()*1 << 0;
+        if (ranpos_ysign == 0) {ranpos_ysign = -1;}
+        
+        pos_y = Math.floor( Math.sqrt( radius*radius - ranpos_x*ranpos_x ) );
+        
+        var astroid = p1_astroids.create(ranpos_x, ranpos_ysign*pos_y, 'astroid');
+    }
+    
+    var astroid = p1_astroids.create(50, 120, 'astroid');
+
 
     // The player and its settings
     //~ aim = game.add.sprite(32, game.world.height - 150, 'dude');
