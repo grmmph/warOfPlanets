30597a770fc53da14855cce239bbd01b1501bdb2
neiman30 <neiman.mail@gmail.com>
2015-11-28 16:59:00 +0100

    add astroid - working

--- frontend/Game.js
+++ frontend/Game.js
@@ -1,10 +1,11 @@
 // consts
-var radius = 80;
+var radius = 96; //radius must be divided by astroid_init_num without a remainder
 var planet1x = 800;
 var planet1y = 300;
 var planet2x = 300;
 var planet2y = 300;
 var astroid_perimeter = 30;
+var astroid_init_num = 6; 
 
 var gameSize = [1000, 800];
 var game = new Phaser.Game(gameSize[0], gameSize[1], Phaser.AUTO, '', { preload: preload, create: create, update: update });
@@ -44,25 +45,30 @@ function create() {
     var planet1 = planets.create(planet1x, planet1y, 'planet1');
     var planet2 = planets.create(planet2x, planet1y, 'planet2');
 
-
     aim1 = game.add.sprite(planet1.x + (planet1.width/2), planet1.y + (planet1.height/2), 'aim');
 		aim1.anchor.setTo(0.5, 0.5);
     //  The astroids group contains the planets (at least two)
     p1_astroids = game.add.group();
     p1_astroids.enableBody = true;
-    for (var i = 0; i < 7; i++)
+    
+    var distance_astroids = (2*radius)/astroid_init_num - 8;
+    for (var i = 0; i < astroid_init_num; i++)
     {
         //  Create an random astroid
-        var ranpos_x = (Math.random()*radius*2 << 0) - radius;
-        var ranpos_ysign = Math.random()*2 << 0;
-        if (ranpos_ysign == 0) {ranpos_ysign = -1;}
-
+        var ranpos_x = (Math.random()*distance_astroids << 0) + i*(distance_astroids+8) - radius;
         pos_y = Math.floor( Math.sqrt( radius*radius - ranpos_x*ranpos_x ) );
 
-        var astroid = p1_astroids.create(planet1x + astroid_perimeter + ranpos_x,planet1y + 35 + ranpos_ysign*pos_y, 'astroid');
+        var astroid = p1_astroids.create(planet1x + astroid_perimeter + ranpos_x,planet1y + 35 + pos_y, 'astroid');
     }
+    
+    for (var i = 0; i < astroid_init_num; i++)
+    {
+        //  Create an random astroid
+        var ranpos_x = (Math.random()*distance_astroids << 0) + i*(distance_astroids+8) - radius;
+        pos_y = Math.floor( Math.sqrt( radius*radius - ranpos_x*ranpos_x ) );
 
-
+        var astroid = p1_astroids.create(planet1x + astroid_perimeter + ranpos_x,planet1y + 35 - pos_y, 'astroid');
+    }
 
     //  We need to enable physics on the player
     //~ game.physics.arcade.enable(player);
